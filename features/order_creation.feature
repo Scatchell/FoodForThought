9b74d381de4e314b129d65a621847045755130e0
Anthony Scatchell <ascatche@thoughtworks.com>
2013-10-09 19:23:23 +0300

    Fixing cucumber test (whoops...)

--- features/order_creation.feature
+++ features/order_creation.feature
@@ -2,8 +2,8 @@ Feature: Creating orders
 
   Scenario: Create a valid order
     Given the item "Beef" exists with a price of 5000
-    And the user is on the create an order page
-    When the user inputs a valid name
-    And selects the "Beef" item
-    And creates the order
-    Then they should see a confirmation page showing the "Beef" item
+    And I sign in with valid credentials
+    And I am on the create an order page
+    When I select the only available item
+    And I create the order
+    Then I should see a confirmation page showing the "Beef" item and my username
--- features/step_definitions/order_creation_steps.rb
+++ features/step_definitions/order_creation_steps.rb
@@ -1,27 +1,37 @@
+include Devise::TestHelpers
+
 Given(/^the item "([^"]*)" exists with a price of (\d+)/) do |name, price|
   item = Item.create!(name: name, price: price, created_at: Time.now, updated_at: Time.now, available: true, item_type: 'meat')
 
   item.save
 end
 
-When(/^the user is on the create an order page$/) do
+When(/^I sign in with valid credentials$/) do
+  @username = 'Anthony'
+  email = 'ascatche@thoughtworks.com'
+  password = 'password'
+  User.new(username: @username, email: email, password: password, password_confirmation: password).save!
+
+  visit '/users/sign_in'
+  fill_in "user_email", :with => email
+  fill_in "user_password", :with => password
+  click_button "Sign in"
+end
+
+When(/^I am on the create an order page$/) do
   visit '/'
 end
 
-When(/^the user inputs a valid name$/) do
-  fill_in('Name', :with => 'Test')
-end
-
-When(/^selects the "([^"]*)" item$/) do |item|
+When(/^I select the only available item$/) do
   check('I want')
 end
 
-When(/^creates the order$/) do
+And(/^I create the order$/) do
   click_button('Create Order')
 end
 
-Then(/^they should see a confirmation page showing the "([^"]*)" item$/) do |item|
-  page.should have_content 'Order created for Test'
-  page.should have_content 'Beef'
+Then(/^I should see a confirmation page showing the "(.*?)" item and my username$/) do |item_name|
+  page.should have_content "Order created for #{@username}"
+  page.should have_content item_name
   page.should have_content 'Total price for this order: 5,000'
 end

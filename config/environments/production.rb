97ec514fbcc695b74efc6e607a90af39e59392a9
Anthony Scatchell <ascatche@thoughtworks.com>
2013-10-18 10:13:35 +0300

    Adding host to fix bug in forgot password link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ FoodForThought::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = {:host => 'http://tw-food-for-thought.herokuapp.com/'}
 end

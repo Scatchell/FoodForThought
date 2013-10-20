78cd763a500cdf077794f8ec80a49618df1a81bf
Anthony Scatchell <ascatche@thoughtworks.com>
2013-10-20 13:03:59 +0300

    Added new item creation logic in items new view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,16 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+toggleItemEntryMethods = function () {
+    if ($("#item_type_text_box").is(':hidden')) {
+        $("#item_type_text_box").show();
+        $("#item_type_text_box #item_item_type").prop('disabled', false);
+        $("#item_type_dropdown").hide();
+        $("#item_type_dropdown #item_item_type").prop('disabled', true);
+    } else {
+        $("#item_type_dropdown").show();
+        $("#item_type_dropdown #item_item_type").prop('disabled', false);
+        $("#item_type_text_box").hide();
+        $("#item_type_text_box #item_item_type").prop('disabled', true);
+    }
+};
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ FoodForThought::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = {:host => 'http://tw-food-for-thought.herokuapp.com/'}
+  config.action_mailer.default_url_options = {:host => 'http://tw-food-for-thought.herokuapp.com'}
 
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
